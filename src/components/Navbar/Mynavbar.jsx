63d8f76e5f4904c7f4d5dd79b18f8fba3a2966db
ricoagustinus123 <rico_a.m@outlook.com>
2021-01-29 09:58:18 +0700

    update button logout

--- src/components/Navbar/Mynavbar.jsx
+++ src/components/Navbar/Mynavbar.jsx
@@ -9,28 +9,53 @@ const Mynavbar = () => {
         <div className="container-navbar">
           <h1 className="brand">Andi
           
-          <span style={{color:"red"}}>Apps</span></h1>
+          <span style={{color:"orange"}}>Apps</span></h1>
+
+         {/* link menu */}
           <ul className="menu">
             <li className="menu-components">
               <Link to="/dashboard" className="sub-menu">
                   <span class="material-icons">home</span>
-                  <span>Dashboard</span>
+                  Dashboard
 
               </Link>
             
             </li>
 
-            <li className="menu-components"><Link to="/brand-events" className="sub-menu"><span class="material-icons">insert_invitation</span>brand events</Link></li>
+            <li className="menu-components">
+              <Link to="/brand-events" className="sub-menu">
+                <span class="material-icons">insert_invitation</span>
+                brand events
+              </Link>
+            </li>
 
-            <li className="menu-components"><Link to="/brand" className="sub-menu">
-            <span class="material-icons">local_mall</span>brand</Link></li>
+            <li className="menu-components">
+              <Link to="/brand" className="sub-menu">
+                <span class="material-icons">local_mall</span>
+                brand
+              </Link>
+            </li>
             
-            <li className="menu-components"><Link to="/fixtures" className="sub-menu">
-            <span class="material-icons">storefront</span>fixtures</Link></li>
+            <li className="menu-components">
+              <Link to="/fixtures" className="sub-menu">
+                <span class="material-icons">storefront</span>
+                fixtures
+              </Link>
+            </li>
             
-            <li className="menu-components"><Link to="/statistic" className="sub-menu"><span class="material-icons">trending_up</span>statistic</Link></li>
+            <li className="menu-components">
+              <Link to="/statistic" className="sub-menu">
+                <span class="material-icons">trending_up</span>
+                statistic
+              </Link>
+            </li>
+          
           </ul>
+        {/* link menu */}
+
+        <button className="logout">logout</button>
         </div>
+
      
 
     </nav>
